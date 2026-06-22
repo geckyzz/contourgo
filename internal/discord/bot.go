66b5369e81be9690b8f0c8811e0512bbf0051cdf
geckyzz <gecko.geckyzz@proton.me>
2026-06-22 07:41:46 +0700

    Bump version to 0.8.5

--- internal/discord/bot.go
+++ internal/discord/bot.go
@@ -13,7 +13,7 @@ import (
 )
 
 var (
-	Version      = "0.8.4"
+	Version      = "0.8.5"
 	CommitSHA    = "unknown"
 	RepoOverride = "" // Can be set via ldflags: -X github.com/geckyzz/contourgo/internal/discord.RepoOverride=owner/repo
 )
